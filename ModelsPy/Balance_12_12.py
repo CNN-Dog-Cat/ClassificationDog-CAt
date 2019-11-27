4dddb591c5279df938457d224b1fee9373a4c9be
Pietro Rignanese <pietro_rignanese@hotmail.it>
2019-11-27 12:46:09 +0100

    Add files via upload

---
+++ ModelsPy/Balance_12_12.py
@@ -0,0 +1,69 @@
+path_train = PATH YOUR DIRECTORY TRAIN BALANCE
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import backend as K
+
+def recall_m(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
+
+def precision_m(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+
+# Initialising the CNN
+model = Sequential()
+
+# Convolution
+model.add(Conv2D(32, (3, 3), input_shape = (50, 50, 3), activation = 'relu'))
+
+# Pooling
+model.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Second convolutional layer
+model.add(Conv2D(32, (3, 3), activation = 'relu'))
+model.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Flattening
+model.add(Flatten())
+
+# Full connection
+model.add(Dense(units = 128, activation = 'relu'))
+model.add(Dense(units = 1, activation = 'sigmoid'))
+
+# Compiling the CNN
+model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy',precision_m, recall_m])
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+training_set = train_datagen.flow_from_directory(path_train,
+                                                 target_size = (50, 50),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+model.fit_generator(training_set,
+                         steps_per_epoch = 8000,
+                         epochs = 20,
+                         validation_steps = 2000)
+
+model.save('Cat_Dog_Balance_12_12') #Salva il modello creato per poterlo utilizzare quando si ha necessità
+# Creazione del json e del file rappresentativi del modello per poterlo riprendere senza effettuare ogni volta il fit
+model_json = model.to_json()
+with open("../Model_12_12/model.json","w") as json_file:
+  json_file.write(model_json)
+
+model.save_weights("../Model_12_12/model.h5")
+print("Modello Salvato!")
